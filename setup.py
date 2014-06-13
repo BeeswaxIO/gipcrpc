8b79fcb4d9809b38d8c2c08b362697a3a2efa261
Tomotaka Ito <tomotaka.ito@gmail.com>
2014-06-13 17:51:37 +0900

    bump version: 0.1.0 => 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gipcrpc',
-    version='0.1.0',
+    version='0.1.1',
     description='RPC framework for parent and children using gipc(gevent)',
     long_description=open('README.rst').read(),
     author='Studio Ousia',
